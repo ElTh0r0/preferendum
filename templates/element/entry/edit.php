73720c4ff69ad161f7f2e8b60c3c9a116d98dcd7
ElTh0r0 <elthoro@gmx.de>
2023-01-03 14:37:53 +0100

    Update CakePHP version; m: 2023

--- templates/element/entry/edit.php
+++ templates/element/entry/edit.php
@@ -6,7 +6,7 @@
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright 2019-2022 github.com/ElTh0r0, github.com/bkis
+ * @copyright 2019-2023 github.com/ElTh0r0, github.com/bkis
  * @license   MIT License (https://opensource.org/licenses/mit-license.php)
  * @link      https://github.com/ElTh0r0/preferendum
  * @version   0.5.0
